3127878d4a6b8e2d235e5ab01d2a9e02eee00402
rogerwschmidt <rogerschmidt@gmail.com>
2017-03-09 10:16:58 -0800

    changes

--- src/server/index.js
+++ src/server/index.js
@@ -11,12 +11,13 @@ app.use(compression());
 app.use(STATIC_PATH, express.static('dist'));
 app.use(STATIC_PATH, express.static('public'));
 
-app.get('*', (req, res) => {
+app.get('/a', (req, res) => {
   res.send(renderApp(APP_NAME));
 });
 
 app.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
+  console.log(process.env.NODE_ENV);
   console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
   '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`);
 });
