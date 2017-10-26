8379bcbf4aeef462f908a1866040b95fb3a25ab8
Heather Arvidson <heather.arvidson@gmail.com>
2017-10-26 11:01:22 -0700

    errors

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,28 @@
 import compression from 'compression';
 import express from 'express';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config';
 import { isProd } from '../shared/util';
 import renderApp from './render-app';
 
 const app = express();
+app.disable('x-powered-by')
 
 app.use(compression());
 app.use(STATIC_PATH, express.static('dist'));
 app.use(STATIC_PATH, express.static('public'));
 
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
-
+//what does app.use look like for my routes?
 
 app.get('*', (req, res) => {
+  console.log('route not found');
   res.send(renderApp(APP_NAME));
 });
 
