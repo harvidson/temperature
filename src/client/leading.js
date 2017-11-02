5e44e9b58ada2166dde423d7020f2e86dea68fc0
Heather Arvidson <heather.arvidson@gmail.com>
2017-11-01 17:39:56 -0700

    few styles

--- src/client/leading.js
+++ src/client/leading.js
@@ -5,7 +5,7 @@ import Event from './event';
 const Leading = ({events}) => {
   return (
     <div>
-      <h2 className="f2 fw3 dark-gray">Leading</h2>
+      <h2 className="f2 fw3 dark-gray">Lead</h2>
       <ul>
         { events.map(event => <Event key={ event.id } event={ event } />) }
       </ul>
--- src/client/writing.js
+++ src/client/writing.js
@@ -5,7 +5,7 @@ const Writing = ({events}) => {
   return (
     <div>
       {console.log(events)}
-      <h2 className="f2 fw3 dark-gray">Writing</h2>
+      <h2 className="f2 fw3 dark-gray">Reflect</h2>
       <ul>
         { events.map(event => <Event key={ event.id } event={ event } />) }
       </ul>
